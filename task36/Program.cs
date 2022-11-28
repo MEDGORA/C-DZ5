d82a901be8b3f5bd1cb91e41828d58094e3b2de7
Boris Dovbysh <boris.dovbysh@gmail.com>
2022-11-28 16:44:18 +0300

    Добавил 36

---
+++ task36/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях(индексах).
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0 */
+
+
+int[] FillArray(int size)
+{
+    int[] arr = new int[size];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-100,101);
+    }
+    return arr;
+}
+
+Console.Write("Введите длину массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+Console.WriteLine(string.Join(", ", array));
+int otvet = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    if (i%2 == 1)
+    {
+    otvet = otvet + array[i];
+    }
+}
+if (otvet == 0)
+{
+    Console.WriteLine("Нет элементов");
+}
+else 
+{
+Console.WriteLine(otvet);
+}
